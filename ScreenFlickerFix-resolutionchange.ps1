ebe54161361ab19aff427f1afc9bd6095561c83d
skoomasteve <43890114+skoomasteve@users.noreply.github.com>
2021-03-08 12:22:47 -0600

    Update ScreenFlickerFix-resolutionchange.ps1

--- ScreenFlickerFix-resolutionchange.ps1
+++ ScreenFlickerFix-resolutionchange.ps1
@@ -1,22 +1,22 @@
-﻿##Steven Soward | 2021 | MIT license || Acknowledgments below
+﻿##Steven Soward | © 2021 | MIT license || Acknowledgments below
  
- ## The way this script works to fix screen flicker is to quickly hop away from the current resolution
- ## and then back to the desired resolution for all connected displays: 
+ ## This script is just a specific configuration of Timothy Mui's Set-ScreenResolutionEx.ps1 script using it to fix screen flicker which is common on windows machines using multiple displays.  
+ ## The way this script works to fix screen flicker is to quickly hop away from the current resolution and then back to the desired resolution for all connected displays: 
  
  ##  how do I set it up for my computer?
  ##  The bottom of this script contains sample configuration for a three monitor setup which happens to be the exact configuration that I use.
- ##  To setup for your purposes, change the first resolution of each display at the bottom of this script to something other than your desired resolution,
- ##  then change the second resolution of each to the desired resolution
+ ##  To setup for your purposes, change the first resolution of each display at the bottom of this script to something other than your desired resolution, then change the second resolution of each to the desired resolution
  
  ## If you have one display, then you only need lines containing -deviceid 0  , two displays? then use  -deviceid 0 and -deviceid 1  , and so on. 
- ## 
- ## For example, if you use two monitors, comment out or delete the lines with -deviceid 2 and make sure the final resolutions 
- ## for devices 0 and 1 are your desired (or current) resolution
+
+ ## For example, if you use two monitors, comment out or delete the lines with -deviceid 2 and make sure the final resolutions for devices 0 and 1 are your desired (or current) resolution, then run the script.  If you don't see an improvement, try different "false resolutions"
  
- ## If you need
- 
+
+ #######################################################################################################
  
  #Timothy Mui's code is below which we will use to set the screen resolutions. Both he and Andy Schneider deserve acknowledgement and thanks for their work creating the resolution script.
+
+ #######################################################################################################
 # ------------------------------------------------------------------------ 
 # NAME: Set-ScreenResolutionEx.ps1 
 # AUTHOR: Timothy Mui (https://github.com/timmui) 
@@ -279,9 +279,9 @@ namespace Resolution
 Add-Type $Code 
 [Resolution.ScreenResolution]::ChangeResolution($width,$height,$DeviceID)  
 } 
-
-#end of Timothy's code
-
+ #######################################################################################################
+#End of Timothy's code
+ #######################################################################################################
 #Our simple flicker fix code is below || This is a three monitor setup each with desired resolutions of 1920x1080 
 
 #False resolutions | (set to something other than the resolution you want) | experiment with different supported resolutions here if the flicker isn't resolved.  | comment out or add extra lines as needed. 
